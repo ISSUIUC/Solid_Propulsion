c16c7a11e3a66583339b1e6c973f079f79048a70
Vimalnath1 <vimalnathsh76@gmail.com>
2025-12-06 18:55:35 -0600

    Need to test this

--- daq_firmware/src/main.rs
+++ daq_firmware/src/main.rs
@@ -1,3 +1,5 @@
+use rppal::spi::{Bus, Mode, Segment, SlaveSelect, Spi};
+
 enum State {
     Inert,
     Standby,
@@ -12,6 +14,12 @@ impl State {
 }
 
 fn main() {
+    let lora_spi=Spi::new(Bus::Spi0,SlaveSelect::Ss2,5000000,Mode::Mode0).unwrap();
+    let mut test=0b00001000;
+    let mut rec=0b00000000;
+    let check_reg=lora_spi.transfer(test,rec).unwrap(); //Reads from address x10, should recieve xFF, Needs to be tested
+    // println!(check_reg);
+    lora_spi.write(0b1000000100001000); //Writes to set to sleep mode Write Format: 1 (For write mode) 7 bits (For address) 8 bits (For Data)
     let mut state = State::new();
 }
 
--- daq_firmware/target/aarch64-unknown-linux-gnu/debug/daq_firmware.d
+++
@@ -1,1 +0,0 @@
-/target/aarch64-unknown-linux-gnu/debug/daq_firmware: /project/src/main.rs
